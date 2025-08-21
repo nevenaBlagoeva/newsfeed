a118d79d80bd5e1dbf02ca4bb19bb9498a05ad53
NDB <nevenad1995@gmail.com>
2025-08-21 12:27:10 +0100

    Update output

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -37,17 +37,4 @@ resource "aws_lambda_function" "main" {
   handler         = "lambda.lambda_handler"
   runtime         = "python3.11"
   source_code_hash = data.archive_file.lambda_zip.output_base64sha256
-  depends_on      = [aws_iam_role_policy_attachment.lambda_policy]
-}
-
-output "terraform_aws_role_output" {
- value = aws_iam_role.lambda_role.name
-}
-
-output "terraform_aws_role_arn_output" {
- value = aws_iam_role.lambda_role.arn
-}
-
-output "terraform_logging_arn_output" {
- value = aws_iam_policy.iam_policy_for_lambda.arn
 }
