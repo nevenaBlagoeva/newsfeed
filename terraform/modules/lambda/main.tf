42efc1bad97490715c7a78f0cd8cbdb597d9527f
NDB <nevenad1995@gmail.com>
2025-08-23 22:18:22 +0100

    Add creds

--- terraform/modules/lambda/main.tf
+++ terraform/modules/lambda/main.tf
@@ -22,21 +22,17 @@ resource "aws_iam_role_policy_attachment" "lambda_policy" {
   role       = aws_iam_role.lambda_role.name
 }
 
-# Lambda function - use local file deployment
+# Lambda function
 resource "aws_lambda_function" "main" {
-  filename         = "${path.module}/${var.function_name}.zip"
   function_name    = var.function_name
   role            = aws_iam_role.lambda_role.arn
+  s3_bucket       = "newsfeed-lambda-artifacts"
+  s3_key          = "${var.function_name}-${var.commit_sha}.zip"
   handler         = var.handler
   runtime          = "python3.11"
   timeout          = 30
-  source_code_hash = local.source_hash
 
   environment {
-    variables = merge(var.environment_variables, {
-      COMMIT_SHA = var.commit_sha
-    })
+    variables = var.environment_variables
   }
-
-  depends_on = [null_resource.lambda_build]
 }
